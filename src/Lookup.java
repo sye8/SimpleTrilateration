e5ea1f002b53a61a769713ab83cc2b4eb7da8078
Sifan YE <sye8@u.rochester.edu>
2017-06-23 16:18:59 +0800

    Update Lookup.java

--- src/Lookup.java
+++ src/Lookup.java
@@ -27,8 +27,8 @@ import com.lemmingapex.trilateration.NonLinearLeastSquaresSolver;
 import com.lemmingapex.trilateration.TrilaterationFunction;
 
 import sye8.utils.Coordinate;
-import sye8.utils.Node;
 import sye8.utils.Maths;
+import sye8.utils.Node;
 
 /**
  * Servlet implementation class Lookup
